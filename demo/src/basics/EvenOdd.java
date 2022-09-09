d88be5b0bacdbbf2b1c8b78806f92750c7de34d7
srikanthpragada <srikanthpragada@gmail.com>
2022-09-09 20:56:19 +0530

    09-SEP

---
+++ demo/src/basics/EvenOdd.java
@@ -0,0 +1,17 @@
+package basics;
+
+import java.util.Scanner;
+
+public class EvenOdd {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		System.out.print("Enter a number :");
+		int num = s.nextInt(); // Read int from keyboard
+
+		if (num % 2 == 0)
+			System.out.println("Even");
+		else
+			System.out.println("Odd");
+	}
+}
--- demo/src/basics/Hello.java
+++ demo/src/basics/Hello.java
@@ -1,10 +1,7 @@
 package basics;
 
 public class Hello {
-
 	public static void main(String[] args) {
-		 
-            System.out.println("Hello World!");
+          System.out.println("Hello World!");
 	}
-
 }
