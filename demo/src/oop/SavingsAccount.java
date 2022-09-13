e340813588e8f9d4eb70e38990190012a381a222
srikanthpragada <srikanthpragada@gmail.com>
2022-09-13 20:57:58 +0530

    13-SEP

--- demo/src/oop/SavingsAccount.java
+++ demo/src/oop/SavingsAccount.java
@@ -1,7 +1,26 @@
 package oop;
 
 public class SavingsAccount {
-	// Instance variables
+	// Instance variables	
+	private int acno;
+	private String ahname;
+	private double balance;
 
-	// Methods
+	// Constructor
+	public SavingsAccount(int no, String name) {
+		acno = no;
+		ahname = name;
+	}
+	
+	public void deposit(double amount) {
+		balance += amount;
+	}
+	
+	public void withdraw(double amount) {
+		balance -= amount;
+	}
+	
+	public double getBalance() {
+		return balance;
+	}
 }
--- demo/src/oop/TestSavingsAccount.java
+++ demo/src/oop/TestSavingsAccount.java
@@ -3,8 +3,14 @@ package oop;
 public class TestSavingsAccount {
 
 	public static void main(String[] args) {
-		 SavingsAccount s = new SavingsAccount();
+		 SavingsAccount s = new SavingsAccount(1, "Steve");
+		 s.deposit(10000);
 		 
+		 SavingsAccount s2 = new SavingsAccount(2, "Tom");
+         s2.deposit(10000);
+         System.out.println(s2.getBalance());
+         
+         System.out.println(10);
 
 	}
 
