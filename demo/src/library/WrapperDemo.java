035f2abec4f747fc03af9eef47a940440f918d39
srikanthpragada <srikanthpragada@gmail.com>
2022-09-27 20:56:02 +0530

    27-SEP

---
+++ demo/src/library/WrapperDemo.java
@@ -0,0 +1,12 @@
+package library;
+
+public class WrapperDemo {
+
+	public static void main(String[] args) {
+		 double a  = 100;
+		 Double obj  = a; // Autoboxing 
+		 System.out.println(obj.getClass());
+		 
+		 double b = obj; // Unboxing 
+	}
+}
