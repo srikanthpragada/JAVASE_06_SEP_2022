0fc32ff17129674dd008ef90d9616de768f437e7
srikanthpragada <srikanthpragada@gmail.com>
2022-09-29 20:40:56 +0530

    29-SEP

---
+++ demo/src/oop/Test.java
@@ -0,0 +1,21 @@
+package oop;
+
+import java.util.Scanner;
+import catalog.*;
+
+public class Test {
+
+	public static void main(String[] args) {
+		 catalog.Book b;  // fully qualified classname 
+		 Publisher p;
+		 Scanner s;
+		 
+		 b = new catalog.Book();
+		 
+		 //Employee e;
+		 
+		 
+
+	}
+
+}
